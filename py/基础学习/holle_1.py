a8ee223f7dac69ee328a8bcb7e11fb23b29fb5e3
godotliukui <167961831+godotliukui@users.noreply.github.com>
2024-04-27 09:22:28 +0800

    Create holle_1.py

---
+++ py/基础学习/holle_1.py
@@ -0,0 +1,17 @@
+#print() 是打印的意思 会打印双引号内的内容
+print("holle")
+
+#\' 会显示\ 后面的符号  双（单）引号内 直接打引号会报错
+print("Let\'s go")
+
+# \n 是换行符号
+print("holle\nword")
+
+
+# 三引号可以多行打印 省略\n
+print("""   sdfdfafsd
+   asdfas
+   asdfasd
+     fa
+    asf
+""")
